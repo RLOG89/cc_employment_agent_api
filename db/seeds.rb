c158e22beaeaeb90b270b102c65b289701ad38bf
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-21 16:12:10 +0000

    changing seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-a# This file should contain all the record creation needed to seed the database with its default values.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
@@ -8,6 +8,13 @@ a# This file should contain all the record creation needed to seed the database 
 
 User.destroy_all
 
-cc_user = User.create({email: "admin@codeclan.com", password: "adminpass", password_confirmation: "adminpass"})
-student_user = User.create({email: "student@gmail.com", password: "studentpass", password_confirmation: "studentpass"})
-
+cc_user = User.create({
+  email: "a@c.com",
+  password: "adminpass",
+  password_confirmation: "adminpass"
+  })
+student_user = User.create({
+  email: "s@gmail.com",
+  password: "studentpass",
+  password_confirmation: "studentpass"
+})
