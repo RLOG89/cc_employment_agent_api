743184cfdad99fca2efcdcf8a0ef4b2b3f1a074d
RLOG89 <ross.loggie@gmail.com>
2017-01-21 16:46:40 +0000

    add users controller:

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ scope path: 'api' do
   resources :employers
 end
 
+resources :users, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
