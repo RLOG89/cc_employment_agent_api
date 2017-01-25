b3a7c86ea0bc751ef6d076c799a4b42accc132ac
RLOG89 <ross.loggie@gmail.com>
2017-01-25 21:03:21 +0000

    try another post

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -1,4 +1,5 @@
 require 'rest_client'
+require 'json'
 
 class SlacksController < ApplicationController
 
@@ -16,13 +17,22 @@ class SlacksController < ApplicationController
     #   content_type: :json,
     #   payload: { test: "this is a test" }.to_json
     # )
-    RestClient::Request.execute(method: :post,
-                               url: url,
-                               payload: slack_params.to_json,
-                               headers: {"Content-Type" => "application/json"}
-                              )
-    render json: "testing testing"
-  end
+  #   RestClient::Request.execute(method: :post,
+  #                              url: url,
+  #                              payload: slack_params.to_json,
+  #                              headers: {"Content-Type" => "application/json"}
+  #                             )
+  #   render json: "testing testing"
+  # end
+  RestClient.post 'https://abot-slack-bot.ngrok.io/jobs',  {
+   company_name: "FanDuel",
+   job_title: "Junior Developer",
+   salary_range_min: 22000,
+   salary_range_max: 22000,
+   application_closing_date: "2017-02-10",
+   application_process: "Apply direct via their website"
+  }.to_json, {content_type: :json, accept: :json}
+end
 
   private
   def slack_params
