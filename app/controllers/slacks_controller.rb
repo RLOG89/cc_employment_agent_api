38cf34182fc8ffca29dc05c800767e32b5e5de30
RLOG89 <ross.loggie@gmail.com>
2017-01-25 20:24:03 +0000

    minor change with restclient post

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -3,7 +3,7 @@ require 'rest_client'
 class SlacksController < ApplicationController
 
   def index
-    RestClient.post 'https://abot-slack-bot.ngrok.io/jobs', "Hello Alex" , {:content_type => :json}
+    RestClient.post 'https://abot-slack-bot.ngrok.io/jobs', "Hello Alex"
     render json: "this worked!"
   end
 
