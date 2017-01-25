f609715fce32fb154c0bcf65748d37e9c76b15d8
RLOG89 <ross.loggie@gmail.com>
2017-01-25 20:17:00 +0000

    testing RestClient

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -1,6 +1,9 @@
+require 'rest_client'
+
 class SlacksController < ApplicationController
 
   def index
+    RestClient.post 'https://abot-slack-bot.ngrok.io/jobs', "Hello Alex" , {:content_type => :json}
     render json: "this worked!"
   end
 
