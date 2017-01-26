4075e35c93e709622c589c0abb4df528749b1bfe
RLOG89 <ross.loggie@gmail.com>
2017-01-26 09:50:03 +0000

    add index route to get data from slack

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -3,6 +3,17 @@ require 'json'
 
 class SlacksController < ApplicationController
 
+  def index
+      puts "receiving:" + slack_params.to_json
+      response = RestClient.get(
+        'https://abot-slack-bot.ngrok.io/jobs',
+        slack_params.to_json,
+        {content_type: :json, accept: :json}
+      )
+      render json: response, status: :created
+    end
+  end
+
   def create
     puts "sending:" + slack_params.to_json
     response = RestClient.post(
